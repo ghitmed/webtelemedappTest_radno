edd7348c2ffd6c67c24af5c1bd94c3391d1fa720
MirkoA7X <118446249+MirkoA7X@users.noreply.github.com>
2023-02-06 15:03:46 +0100

    Evo vam

---
+++ src/main/java/com/webtelemedapp/webtelemedapp/WebtelemedappApplication.java
@@ -0,0 +1,13 @@
+package com.webtelemedapp.webtelemedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtelemedappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtelemedappApplication.class, args);
+	}
+
+}
